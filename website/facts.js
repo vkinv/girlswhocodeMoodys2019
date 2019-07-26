9c57dec5075a8a454c5db7674df07bd257ccb04e
vkinv <52293001+vkinv@users.noreply.github.com>
2019-07-26 10:07:35 -0700

    Delete facts.js

--- website/facts.js
+++
@@ -1,5 +0,0 @@
-$('.carousel-inner').each(function() {
-
-if ($(this).children('div').length === 1) $(this).siblings('.carousel-indicators, .carousel-control-prev, .carousel-control-next').hide();
-
-});
